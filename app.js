1d5845a89619ded107e3c5b30bf141899944778e
jsrayanyaqoob <rayanyaqob83@gmail.com>
2024-09-08 15:54:08 +0500

    Calculator

---
+++ app.js
@@ -0,0 +1,22 @@
+// Calculator Programming
+
+
+var display = document.querySelector("#display");
+
+
+function displayForInput(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
