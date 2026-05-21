dc8e3d445a1166651ecf1fda789994a790b621ce
jsrayanyaqoob <rayanyaqoob83@gmail.com>
2026-05-21 17:55:46 +0500

    Calculator

--- app.js
+++ app.js
@@ -3,15 +3,51 @@
 
 var display = document.querySelector("#display");
 
-
-
 function displayForInput(input){
+
+    var lastChar = display.value[display.value.length - 1];
+
+    
+    if(input == "."){
+
+        var lastNumber = "";
+
+        for(var i = display.value.length - 1; i >= 0; i--){
+
+            if(
+                display.value[i] == "+" ||
+                display.value[i] == "-" ||
+                display.value[i] == "*" ||
+                display.value[i] == "/"
+            ){
+                break;
+            }
+
+            lastNumber += display.value[i];
+        }
+
+        if(lastNumber.includes(".")){
+            return;
+        }
+    }
+
+
+    // Prevent multiple operators
+    if(
+        (input == "+" || input == "-" || input == "*" || input == "/")
+        &&
+        (lastChar == "+" || lastChar == "-" || lastChar == "*" || lastChar == "/")
+    ){
+        return;
+    }
+
+
     display.value += input;
 }
 
 function clearDisplay(){
     display.value = "";
-}
+} 
 
 function calculate(){
     try{
